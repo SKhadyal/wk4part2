3258434b668223c7f1ea3953120c50f95ec1ef66
SKhadyal <sushanksharma006@gmail.com>
2025-08-25 13:32:27 +1000

    Commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,13 @@
+// app/page.tsx
+import React from 'react';
+import BlogList from './Components/BlogList';
+
+const App = () => {
+  return (
+    <div>
+      <BlogList />
+    </div>
+  );
+};
+
+export default App;
